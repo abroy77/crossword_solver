4ca966cd2b8f6a93763f8e3e976979ada7804903
Abhishek Roy <abhishek.roy@perspectum.com>
2023-06-07 09:31:02 +0100

    build crossword and variable classes

--- src/crossword_builder/build_crossword.py
+++ src/crossword_builder/build_crossword.py
@@ -3,26 +3,68 @@ from typing import List, Tuple, Dict, Optional, Union
 from collections import namedtuple
 import pandas as pd
 from os import path
+from itertools import permutations
 
 ACROSS = 'across'
 DOWN = 'down'
 
 Position = namedtuple('Position', ['i', 'j'])
-BoardVariable = namedtuple('BoardVariable', ['i', 'j', 'index', 'orientation', 'length'])
-PromptVariable = namedtuple('PromptVariable', ['orientation', 'index', 'prompt', 'length'])
+BoardVariable = namedtuple(
+    'BoardVariable', ['i', 'j', 'index', 'orientation', 'length'])
+PromptVariable = namedtuple(
+    'PromptVariable', ['orientation', 'index', 'prompt', 'length'])
 
 
 class Clue():
     pass
 
+# declare variable class
+
+
+class Variable():
+    pass
+
 
 class Crossword():
-    pass
+    def __init__(self, board_filepath: str, prompts_filepath: str) -> None:
+        self.board, variable_positions = read_board(board_filepath)
+        prompts = read_prompts(prompts_filepath)
+        self.variables = combine_board_and_prompts(variable_positions, prompts)
+        self.setup_intersections()
+
+    def setup_intersections(self):
+        self.intersections = dict()
+        # loop through all the variables
+        for v1, v2 in permutations(self.variables, 2):
+            v1_cells = v1.cells
+            v2_cells = v2.cells
+            # check if they intersect
+            intersection = set(v1_cells).intersection(set(v2_cells))
+            if intersection:
+                # add to the intersections dict
+                intersection = intersection.pop()
+                self.intersections[v1, v2] = (
+                    v1_cells.index(intersection),
+                    v2_cells.index(intersection)
+                )
+
+    def neighbours(self, var: Variable) -> List[Variable]:
+        neighbours = []
+        for v1, v2 in self.intersections.keys():
+            if v1 == var:
+                neighbours.append(v2)
+            elif v2 == var:
+                neighbours.append(v1)
+        return neighbours
 
 
 class Variable():
 
-    def __init__(self, pos: Position, index: int, orientation: str, length: Union[int, Tuple[int, int]], prompt: str) -> None:
+    def __init__(self,
+                 pos: Position,
+                 index: int,
+                 orientation: str,
+                 length: Union[int, Tuple[int, int]], prompt: str) -> None:
         # let's setup the values
         self.pos = pos
         self.index = index
@@ -45,7 +87,11 @@ class Variable():
         if isinstance(self.length, int) and self.length < 3:
             print("length < 3")
             return False
-        if isinstance(self.length, tuple) and (self.length[0] + self.length[1] < 3):
+        if isinstance(
+            self.length,
+            tuple) and (
+            self.length[0] +
+                self.length[1] < 3):
             print("length < 3")
             return False
         # check the prompt
@@ -53,7 +99,13 @@ class Variable():
             print("len(prompt) < 1")
             return False
         # check i, j, index are ints
-        if not isinstance(self.pos.i, int) or not isinstance(self.pos.i, int) or not isinstance(self.index, int):
+        if not isinstance(
+                self.pos.i,
+                int) or not isinstance(
+                self.pos.i,
+                int) or not isinstance(
+                self.index,
+                int):
             print("i, j, index not ints")
             return False
         return True
@@ -61,19 +113,24 @@ class Variable():
     def setup_cells(self) -> None:
         # setup the cells
         if self.orientation == ACROSS:
-            self.cells = [(self.i, self.pos.j + k) for k in range(self.length)]
+            self.cells = [(self.pos.i, self.pos.j + k)
+                          for k in range(self.length)]
         elif self.orientation == DOWN:
-            self.cells = [(self.pos.i + k, self.pos.j) for k in range(self.length)]
+            self.cells = [(self.pos.i + k, self.pos.j)
+                          for k in range(self.length)]
         return
 
     def __str__(self) -> str:
         return self.__repr__()
 
     def __repr__(self) -> str:
-        return f'Variable(coordinate: ({self.pos.i}, {self.pos.j}),\norientation: {self.orientation},\nlength: {self.length},\nprompt {self.prompt})'
+        return f'Variable(coordinate: ({self.pos.i}, {self.pos.j}),\
+                    \norientation: {self.orientation},\
+                    \nlength: {self.length},\
+                    \nprompt: {self.prompt})'
 
     def __hash__(self) -> int:
-        hash((self.pos, self.orientation, self.length))
+        return hash((self.pos, self.orientation, self.length))
 
     def __eq__(self, other) -> bool:
         return self.__hash__() == other.__hash__()
@@ -86,13 +143,13 @@ def read_board(filepath: str) -> List[List[str]]:
         cols = len(board[0])
         length = 1
         if orientation == ACROSS:
-            for j in range(pos.j+1, cols):
+            for j in range(pos.j + 1, cols):
                 if board[pos.i][j]:
                     length += 1
                 else:
                     break
         elif orientation == DOWN:
-            for i in range(pos.i+1, rows):
+            for i in range(pos.i + 1, rows):
                 if board[i][pos.j]:
                     length += 1
                 else:
@@ -102,9 +159,9 @@ def read_board(filepath: str) -> List[List[str]]:
     def is_start_of_variable(board, pos) -> List:
 
         starts = []
-        if board[pos.i][pos.j] and (pos.i == 0 or not board[pos.i-1][pos.j]):
+        if board[pos.i][pos.j] and (pos.i == 0 or not board[pos.i - 1][pos.j]):
             starts.append(DOWN)
-        if board[pos.i][pos.j] and (pos.j == 0 or not board[pos.i][pos.j-1]):
+        if board[pos.i][pos.j] and (pos.j == 0 or not board[pos.i][pos.j - 1]):
             starts.append(ACROSS)
         return starts
 
@@ -129,14 +186,16 @@ def read_board(filepath: str) -> List[List[str]]:
             appended = False
             if is_start_of_variable(board, Position(i, j)):
                 for orientation in is_start_of_variable(board, Position(i, j)):
-                    length = get_variable_length(board, Position(i, j), orientation)
+                    length = get_variable_length(
+                        board, Position(i, j), orientation)
                     if length > 1:
-                        board_variables.append(BoardVariable(i, j, index, orientation, length))
+                        board_variables.append(BoardVariable(
+                            i, j, index, orientation, length))
                         appended = True
                 if appended:
                     index += 1
 
-    return board_variables
+    return board, board_variables
 
 
 def read_prompts(filepath: str) -> List[PromptVariable]:
@@ -159,11 +218,14 @@ def read_prompts(filepath: str) -> List[PromptVariable]:
         # handle orientations
         orientation = DOWN if row['orientation'] == 'D' else ACROSS
 
-        prompts.append(PromptVariable(orientation, index, row['prompt'], length))
+        prompts.append(PromptVariable(
+            orientation, index, row['prompt'], length))
     return prompts
 
 
-def match_prompt_to_position(board_var: BoardVariable, prompt: PromptVariable) -> bool:
+def match_prompt_to_position(
+        board_var: BoardVariable,
+        prompt: PromptVariable) -> bool:
     index = board_var.index
     orientation = board_var.orientation
     # find corresponding prompt
@@ -171,13 +233,22 @@ def match_prompt_to_position(board_var: BoardVariable, prompt: PromptVariable) -
         return True
 
 
-def combine_board_and_prompts(board_vars: List[BoardVariable], prompts: List[PromptVariable]) -> List[Variable]:
+def combine_board_and_prompts(
+        board_vars: List[BoardVariable],
+        prompts: List[PromptVariable]) -> List[Variable]:
     variables = []
     for board_var in board_vars:
         for prompt in prompts:
             if match_prompt_to_position(board_var, prompt):
-                variables.append(Variable(Position(board_var.i, board_var.j), board_var.index,
-                                 board_var.orientation, board_var.length, prompt.prompt))
+                variables.append(
+                    Variable(
+                        Position(
+                            board_var.i,
+                            board_var.j),
+                        board_var.index,
+                        board_var.orientation,
+                        board_var.length,
+                        prompt.prompt))
 
     return variables
 
@@ -188,11 +259,14 @@ def main():
     board_filepath = path.join('puzzles', puzzle, 'board.txt')
     prompts_filepath = path.join('puzzles', puzzle, 'prompts.csv')
 
-    variable_positions = read_board(board_filepath)
-    prompts = read_prompts(prompts_filepath)
-    variables = combine_board_and_prompts(variable_positions, prompts)
-    for variable in variables:
-        print(variable)
+    # variable_positions = read_board(board_filepath)
+    # prompts = read_prompts(prompts_filepath)
+    # variables = combine_board_and_prompts(variable_positions, prompts)
+    # for variable in variables:
+    #     print(variable)
+    # return
+
+    crossword = Crossword(board_filepath, prompts_filepath)
     return
 
 
